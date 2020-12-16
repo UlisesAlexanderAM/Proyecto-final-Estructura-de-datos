301e9706b8cc17980a67cb34c550b1a42f721fea
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-12-15 18:16:37 -0600

    Implementación operación pop de una pila.

--- ProyectoFinalEstructuraDeDatos/src/pilaCola/pila.java
+++ ProyectoFinalEstructuraDeDatos/src/pilaCola/pila.java
@@ -31,4 +31,25 @@ public class pila<T> extends listaDoble<T> {
 		insertar(new nodoLista<>(dato));
 	}
 
+	public T pop(){
+		nodoLista<T> nodoTmp = cola.getPrevio().getPrevio();
+		T dato = cola.getPrevio().getDato();
+		nodoTmp.setSiguiente(cola);
+		cola.setPrevio(nodoTmp);
+		return dato;
+	}
+
+	public void eliminar() {
+		nodoLista<T> nodoTmp = cola.getPrevio().getPrevio();
+		nodoTmp.setSiguiente(cola);
+		cola.setPrevio(nodoTmp);
+	}
+
+	public void pop(int opcion){
+		if (opcion == 1){
+			pop();
+		}
+		else
+			eliminar();
+	}
 }
