1fdddf4fcec4dd674f4e79ce22ee18f02db649dc
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-12-18 11:38:34 -0600

    Se crea la ventana del menu.

--- ProyectoFinalEstructuraDeDatos/src/InterfazGrafica/Menu.java
+++ ProyectoFinalEstructuraDeDatos/src/InterfazGrafica/Menu.java
@@ -1,7 +1,21 @@
 package InterfazGrafica;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Menu extends JFrame {
 	protected static final JFrame ventana =new JFrame();
+
+	public static void main(String[] args) {
+		JPanel ventanaContenedor = new JPanel();
+
+
+		ventana.setTitle("Menu Listas");
+		ventana.setContentPane(ventanaContenedor);
+		ventana.setLayout(new GridBagLayout());
+		ventana.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		ventana.setVisible(true);
+		ventana.pack();
+
+	}
 }
